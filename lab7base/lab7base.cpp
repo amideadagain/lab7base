428e4951f5d60b2b7e3da6f95e9d2dd74b37346d
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:50:58 +0300

    Добавьте файлы проекта.

---
+++ lab7base/lab7base.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <stack>
+
+struct Book
+{
+	std::string name;
+	int count;
+	void show()
+	{
+		std::cout << "Name: " << name;
+		std::cout << "\nPages: " << count << '\n';
+	}
+};
+int main()
+{
+	std::stack<Book> books;
+	books.push({ "Qwe",4 });
+	books.push({ "Qwasfe",44 });
+	books.push({ "Qwsfae",45 });
+	std::cout << "book count =" << books.size() << '\n';
+
+	while (!books.empty())
+	{
+		auto temp = books.top();
+		books.pop();
+		temp.show();
+	}
+}
